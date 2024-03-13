25c1188f4108c3d049c4ba9367db799c142d3441
jinss5 <jinss1145@gmail.com>
2024-03-12 22:53:01 -0500

    add parsing

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import sys
 
-def parser(input):
-    return input
+def parse_diff(diff_stream):
+    diff_data = []
+    current_file = None
+    current_hunk = None
+    for line in diff_stream:
+        if line.startswith('+++ ') or line.startswith('--- '):
+            if current_file:
+                diff_data.append(current_file)
+            current_file = {'filename': line[4:].strip(), 'changes': []}
+        elif line.startswith('@@'):
+            current_hunk = {'hunk': line.strip(), 'added': [], 'removed': []}
+            current_file['changes'].append(current_hunk)
+        # code that had been added
+        elif line.startswith('+') and line[1:].strip() != '':
+            current_hunk['added'].append(line[1:].strip())
+        # code that had been deleted
+        elif line.startswith('-') and line[1:].strip() != '':
+            current_hunk['removed'].append(line[1:].strip())
+    if current_file:
+        diff_data.append(current_file)
+    return diff_data
 
-def main(input_stream):
 
-    changes = parser(input_stream)
+def main(diff_stream):
 
-    for line in changes:
+    print(parse_diff(diff_stream))
 
-        print(line, end='')
 
 if __name__ == "__main__":
     main(sys.stdin)
